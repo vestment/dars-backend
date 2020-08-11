0876ac4943a8513b4d55e355949e4017c02a4cf8
Anwar Saeed <Anwarsaaed1@yahoo.com>
2020-08-11 11:05:36 +0200

    nav mobile

--- resources/views/frontend/layouts/app1.blade.php
+++ resources/views/frontend/layouts/app1.blade.php
@@ -397,8 +397,8 @@
 
 
 
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
-    <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet"/>
+    <!-- <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script> -->
+    <!-- <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet"/> -->
   
 
 
